17ac4ab63efa49cc6eaada2b9658cdaf5500f553
Ashiq Sabith <ashiqsabith328@gmail.com>
2021-06-23 15:35:18 +0600

    sign in page fully completed

---
+++ script.js
@@ -0,0 +1,42 @@
+function clicked(){
+    var a = document.getElementById("alert");
+    a.style.display = 'block';
+}
+
+function clicke(){
+    var b = document.getElementById("alert1");
+    b.style.display = 'block';
+}
+
+function valu(){
+    var user = document.getElementById("username").value
+    var pass = document.getElementById("password").value
+    
+    
+
+    if(user==""){
+        console.log(user);
+        var ex = document.getElementById("ecxl");
+        ex.style.display = 'block';
+    }
+
+    else{
+        var ex = document.getElementById("ecxl");
+        ex.style.display = 'none'; 
+        console.log("no")
+    }
+
+    if(pass==""){
+        console.log(user);
+        var ex = document.getElementById("ecxl1");
+        ex.style.display = 'block';
+    }
+
+    else{
+        var ex = document.getElementById("ecxl1");
+        ex.style.display = 'none'; 
+        console.log("no")
+    }
+
+
+}
